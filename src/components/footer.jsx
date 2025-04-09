da8db089f3b84d09c1fbd2b46f9daf64859cd254
sandeepHiscope <sandeep.m@hiscopetech.com>
2025-04-09 07:33:13 +0530

    styled footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -17,30 +17,29 @@ const Footer = () => {
           <img src={VDrLogo} alt="VDr Logo" className="footer-logo" />
         </Link>
       </div>
-      
 
-      <div className="footer-bottom">
-      <p className="footer-description">
-        VDr is committed to connecting you with top healthcare professionals.
-      </p>
+      <div className="footer-text-content">
+        <p className="footer-description">
+          VDr is committed to connecting you with top healthcare professionals.
+        </p>
         <p>&copy; {new Date().getFullYear()} VDr. All Rights Reserved.</p>
       </div>
-      <div className="footer-social">
-        <div className="social-icons">
-          <a href="https://facebook.com" target="_blank" rel="noreferrer">
-            <FaFacebookF />
-          </a>
-          <a href="https://twitter.com" target="_blank" rel="noreferrer">
-            <FaTwitter />
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-            <FaLinkedinIn />
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noreferrer">
-            <FaInstagram />
-          </a>
-        </div>
+
+      <div className="social-icons">
+        <a href="https://facebook.com" target="_blank" rel="noreferrer">
+          <FaFacebookF />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noreferrer">
+          <FaTwitter />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+          <FaLinkedinIn />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <FaInstagram />
+        </a>
       </div>
+      <button className="to-top-btn" onClick={() => window.scrollTo(0, 0)}>🔝</button>
     </footer>
   );
 };
