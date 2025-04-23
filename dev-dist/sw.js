f04cc7d62f3965d4b449e6dcfb82bcefc4ad310c
anjaneyulu25 <anjaneyulu.k@hiscopetech.com>
2025-04-23 11:41:12 +0530

    updated vrifydoc

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,8 +79,7 @@ define(['./workbox-e328c5da'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-
-    "revision": "0.tvrm8m93i1"
+    "revision": "0.eu4oa3706qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
