518173899721e960b41d19e205394dbc9d39d121
kiranvenkatbeesetty <kiran.b@hiscopetech.com>
2025-04-23 14:54:39 +0530

    changed the footer of the app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,10 +79,7 @@ define(['./workbox-e328c5da'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-
-
-    "revision": "0.imqagvaqon"
-
+    "revision": "0.5s80m5nqu8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
